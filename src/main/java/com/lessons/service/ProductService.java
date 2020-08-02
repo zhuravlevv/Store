46bf068eef38bc4bef71834316d8cdbf3579dcc8
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-08-02 23:30:18 +0300

    ProductService

--- src/main/java/com/lessons/service/ProductService.java
+++ src/main/java/com/lessons/service/ProductService.java
@@ -1,4 +1,18 @@
 package com.lessons.service;
 
+import com.lessons.entity.Product;
+
+import java.util.List;
+
 public interface ProductService {
+
+    public List<Product> getAll();
+
+    public Product getById(Integer id);
+
+    public Product save(Product product);
+
+    public Product update(Product newProduct, Integer id);
+
+    public void delete(Integer id);
 }
---
+++ src/main/java/com/lessons/service/impl/ProductServiceImpl.java
@@ -0,0 +1,6 @@
+package com.lessons.service.impl;
+
+import com.lessons.service.ProductService;
+
+public class ProductServiceImpl implements ProductService {
+}
