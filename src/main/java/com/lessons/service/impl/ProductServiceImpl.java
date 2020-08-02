a6003d94043415659968b21db4e1cb6f2db9b8a4
Vlad Zhuravlev <vlad12.09.01.1@mail.ru>
2020-08-02 23:32:38 +0300

    ProductServiceImpl

--- src/main/java/com/lessons/service/impl/ProductServiceImpl.java
+++ src/main/java/com/lessons/service/impl/ProductServiceImpl.java
@@ -1,6 +1,47 @@
 package com.lessons.service.impl;
 
+import com.lessons.entity.Product;
+import com.lessons.repository.ProductRepository;
 import com.lessons.service.ProductService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor(onConstructor = @__({@Autowired}))
 public class ProductServiceImpl implements ProductService {
+
+    private final ProductRepository productRepository;
+
+    @Override
+    public List<Product> getAll() {
+        return productRepository.findAll();
+    }
+
+    @Override
+    public Product getById(Integer id) {
+        try {
+            return productRepository.findById(id).orElseThrow(Exception::new);
+        } catch (Exception e){
+            System.out.println("Product with id = " + id + " doesn't exist.");
+        }
+        return null;
+    }
+
+    @Override
+    public Product save(Product product) {
+        return productRepository.save(product);
+    }
+
+    @Override
+    public Product update(Product newProduct, Integer id) {
+        return null;
+    }
+
+    @Override
+    public void delete(Integer id) {
+        productRepository.deleteById(id);
+    }
 }
